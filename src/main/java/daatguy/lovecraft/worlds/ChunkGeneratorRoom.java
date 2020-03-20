ee9dc27b2d47a726d7f0e3e77d2645b19e665668
Kai Hoop <unrelatedstring@gmail.com>
2020-03-20 06:41:17 -0400

    There's a shitty room in the room dimension
    Teleport to 4 101 4 for best results

--- src/main/java/daatguy/lovecraft/worlds/ChunkGeneratorRoom.java
+++ src/main/java/daatguy/lovecraft/worlds/ChunkGeneratorRoom.java
@@ -3,12 +3,14 @@ package daatguy.lovecraft.worlds;
 import java.util.List;
 
 import net.minecraft.entity.EnumCreatureType;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome.SpawnListEntry;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.ChunkPrimer;
 import net.minecraft.world.gen.IChunkGenerator;
+import net.minecraft.block.state.IBlockState;
 
 public class ChunkGeneratorRoom implements IChunkGenerator {
 	
@@ -25,9 +27,38 @@ public class ChunkGeneratorRoom implements IChunkGenerator {
 	}
 
 	@Override
-	public void populate(int x, int z) {
-		// TODO Auto-generated method stub
-
+	public void populate(int chunkX, int chunkZ) {
+		if (chunkX != 0 || chunkZ != 0) {
+			return;
+		}
+		
+		final int SIDE_LENGTH = 9;
+		final int ELEVATION = 100;
+		final IBlockState BEDROCK = Blocks.BEDROCK.getDefaultState();
+		
+		
+		for (int x = 0; x < SIDE_LENGTH; x++) {
+			for (int z = 0; z < SIDE_LENGTH; z++) {
+				world.setBlockState(new BlockPos(x, ELEVATION, z), BEDROCK);
+			}
+		}
+		
+		for (int y = 1; y < SIDE_LENGTH - 1; y++) {
+			for (int x = 0; x < SIDE_LENGTH; x++) {
+				world.setBlockState(new BlockPos(x, y + ELEVATION, 0), BEDROCK);
+				world.setBlockState(new BlockPos(x, y + ELEVATION, SIDE_LENGTH - 1), BEDROCK);
+				for (int z = 1; z < SIDE_LENGTH - 1; z++) {
+					world.setBlockState(new BlockPos(0, y + ELEVATION, z), BEDROCK);
+					world.setBlockState(new BlockPos(SIDE_LENGTH - 1, y + ELEVATION, z), BEDROCK);
+				}
+			}
+		}
+		
+		for (int x = 0; x < SIDE_LENGTH; x++) {
+			for (int z = 0; z < SIDE_LENGTH; z++) {
+				world.setBlockState(new BlockPos(x, ELEVATION + SIDE_LENGTH - 1, z), BEDROCK);
+			}
+		}
 	}
 
 	@Override
