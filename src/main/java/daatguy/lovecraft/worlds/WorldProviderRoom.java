a3cd60743ddff2c49f8d699955b164bb0567b4c6
Kai Hoop <unrelatedstring@gmail.com>
2020-03-21 22:11:22 -0400

    no weird red stuff in the room void it was just bugging me

--- src/main/java/daatguy/lovecraft/worlds/WorldProviderRoom.java
+++ src/main/java/daatguy/lovecraft/worlds/WorldProviderRoom.java
@@ -8,6 +8,9 @@ import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.gen.IChunkGenerator;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.Nullable;
+
 import daatguy.lovecraft.core.LovecraftMain;
 
 public class WorldProviderRoom extends WorldProvider {
@@ -42,6 +45,18 @@ public class WorldProviderRoom extends WorldProvider {
 	public Vec3d getSkyColor(Entity entity, float partialTicks) {
 		return new Vec3d(1D, 1D, 1D);
 	}
+	
+	@Nullable
+    @SideOnly(Side.CLIENT)
+    public float[] calcSunriseSunsetColors(float celestialAngle, float partialTicks)
+    {
+        return null;
+    }
+	
+	public float calculateCelestialAngle(long worldTime, float partialTicks)
+    {
+        return 0.0F;
+    }
 	
 	protected void generateLightBrightnessTable() {
 		for (int i = 0; i < 8; i++) {
