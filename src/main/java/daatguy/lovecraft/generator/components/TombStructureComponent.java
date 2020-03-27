f0a4ef80264a3130a1320815d5c7447a8042903f
Kai Hoop <unrelatedstring@gmail.com>
2020-03-27 03:04:52 -0400

    Nope they still generate fine, anyhow I'm also using the given Random
    We probably want to use that for the sake of consistency between two
    runs of the same seed or whatever (and it's also more readable than the
    whole casting a multiplied float to int mess

--- src/main/java/daatguy/lovecraft/generator/components/TombStructureComponent.java
+++ src/main/java/daatguy/lovecraft/generator/components/TombStructureComponent.java
@@ -123,7 +123,7 @@ public class TombStructureComponent extends StructureComponent {
 				z), this.getYWithOffset(y), this.getZWithOffset(x, z),
 				new ResourceLocation("lovecraft:chests/tomb"));
 		this.placeCarving(worldIn, 3, 0, 5, EnumFacing.UP, "carving.tomb"
-				+ String.valueOf((int) (Math.random() * 5)), (int) (Math.random() * 4));
+				+ String.valueOf(randomIn.nextInt(5)), randomIn.nextInt(4));
 		return true;
 	}
 
